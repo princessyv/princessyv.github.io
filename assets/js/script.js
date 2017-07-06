1121971d2cfba77c518fb16c1f2fb7ba384fe941
ifamille <jianguo.annonce@gmail.com>
2017-07-06 14:27:07 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -12238,8 +12238,9 @@ return jQuery;
       // open modal when `s` button is pressed
       $(document).keyup(function(event) {
         var target = event.target || event.srcElement;
-        // exit if user is focusing an input
-        if (target.tagName.toUpperCase() === 'INPUT') {
+        // exit if user is focusing an input or textarea
+        var tagName = target.tagName.toUpperCase();
+        if (tagName === 'INPUT' || tagName === 'TEXTAREA') {
           return;
         }
 
@@ -12322,7 +12323,7 @@ return jQuery;
         html += '<div class="media">';
         if (post.thumbnailImageUrl) {
           html += '<div class="media-left">';
-          html += '<a class="link-unstyled" href="' + (post.link || post.permalink) +'">';
+          html += '<a class="link-unstyled" href="' + (post.link || post.permalink) + '">';
           html += '<img class="media-image" ' +
             'src="' + post.thumbnailImageUrl + '" ' +
             'width="90" height="90"/>';
@@ -12331,7 +12332,7 @@ return jQuery;
         }
 
         html += '<div class="media-body">';
-        html += '<a class="link-unstyled" href="' + (post.link || post.permalink) +'">';
+        html += '<a class="link-unstyled" href="' + (post.link || post.permalink) + '">';
         html += '<h3 class="media-heading">' + post.title + '</h3>';
         html += '</a>';
         html += '<span class="media-meta">';
@@ -12418,9 +12419,9 @@ return jQuery;
 })(jQuery);
 ;(function($) {
   'use strict';
-
+  
   // Open and close the share options bar
-
+  
   /**
    * ShareOptionsBar
    * @constructor
@@ -12428,18 +12429,19 @@ return jQuery;
   var ShareOptionsBar = function() {
     this.$shareOptionsBar = $('#share-options-bar');
     this.$openBtn = $('.btn-open-shareoptions');
-    this.$closeBtn = $('#share-options-mask');
+    this.$closeBtn = $('#btn-close-shareoptions');
+    this.$body = $('body');
   };
-
+  
   ShareOptionsBar.prototype = {
-
+    
     /**
      * Run ShareOptionsBar feature
      * @return {void}
      */
     run: function() {
       var self = this;
-
+      
       // Detect the click on the open button
       self.$openBtn.click(function() {
         if (!self.$shareOptionsBar.hasClass('opened')) {
@@ -12447,7 +12449,7 @@ return jQuery;
           self.$closeBtn.show();
         }
       });
-
+      
       // Detect the click on the close button
       self.$closeBtn.click(function() {
         if (self.$shareOptionsBar.hasClass('opened')) {
@@ -12456,50 +12458,50 @@ return jQuery;
         }
       });
     },
-
+    
     /**
      * Open share options bar
      * @return {void}
      */
     openShareOptions: function() {
       var self = this;
-
+      
       // Check if the share option bar isn't opened
       // and prevent multiple click on the open button with `.processing` class
       if (!self.$shareOptionsBar.hasClass('opened') &&
         !this.$shareOptionsBar.hasClass('processing')) {
         // Open the share option bar
         self.$shareOptionsBar.addClass('processing opened');
-
+        self.$body.css('overflow', 'hidden');
+        
         setTimeout(function() {
           self.$shareOptionsBar.removeClass('processing');
         }, 250);
       }
     },
-
+    
     /**
      * Close share options bar
      * @return {void}
      */
     closeShareOptions: function() {
       var self = this;
-
+      
       // Check if the share options bar is opened
       // and prevent multiple click on the close button with `.processing` class
       if (self.$shareOptionsBar.hasClass('opened') &&
         !this.$shareOptionsBar.hasClass('processing')) {
         // Close the share option bar
-        self.$shareOptionsBar
-          .addClass('processing')
-          .removeClass('opened');
-
+        self.$shareOptionsBar.addClass('processing').removeClass('opened');
+        
         setTimeout(function() {
           self.$shareOptionsBar.removeClass('processing');
+          self.$body.css('overflow', '');
         }, 250);
       }
     }
   };
-
+  
   $(document).ready(function() {
     var shareOptionsBar = new ShareOptionsBar();
     shareOptionsBar.run();
